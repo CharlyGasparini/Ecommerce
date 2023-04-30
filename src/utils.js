292225784cf232319c0d41d0051068a95db75e98
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-04-30 12:17:13 -0300

    configuración multer y path absoluto

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+import { fileURLToPath } from "url";
+import { __dirname } from "path";
+import multer from "multer";
+
+// Convención nomenclatura: para temas de paths __nombre
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = __dirname(__filename);
+
+// Implementación de multer
+// Almacenamiento:
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, `${__dirname}/public/img`);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+// Middleware:
+const uploader = multer({
+    storage,
+    onError: (err, next) => {
+        console.log(err),
+        next();
+    }
+});
+
+export {
+    __dirname,
+    uploader
+};
