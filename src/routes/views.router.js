7f9b258c0ccd8c6b46d05cb885c0dd3f8c3c5b91
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-05-12 09:39:55 -0300

    modif: cambié el import de ProductManager a dbProductManager

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import ProductManager from "../managers/ProductManager.js";
+import dbProductManager from "../dao/dbManagers/dbProductManager.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
     res.render("index", {
         title: "Planilla de productos",
-        products: await new ProductManager("./src/files/products.json").getProducts()
+        products: await new dbProductManager().getProducts(),
     });
 });
 
