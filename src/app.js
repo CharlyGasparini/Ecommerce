b145282b3564e852a1fabf459cef3a04a577abb7
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-05-14 11:26:14 -0300

    app.set socketio

--- src/app.js
+++ src/app.js
@@ -9,6 +9,35 @@ import dbProductManager from "./dao/dbManagers/dbProductManager.js";
 import mongoose from "mongoose";
 
 const app = express(); // Creación de server HTTP
+const httpServer = app.listen(8080, () => console.log("Listening on port 8080"));
+const io = new Server(httpServer); // Creación de server Websockets
+// Conexión a base de datos con mongoose
+try {
+    await mongoose.connect("mongodb+srv://crgasparini32:GSNltCfSNvAcw1xd@cluster39760cg.jgxfm1z.mongodb.net/ecommerce?retryWrites=true&w=majority")
+} catch (error) {
+    console.log(error);
+}
+// Handshake entre servidor y socket del cliente
+io.on("connection", async socket => {
+    console.log("Nuevo cliente conectado");
+    const manager = new dbProductManager();
+    try {
+        const products = await manager.getProducts();
+        socket.emit("showProducts", products); // Envia los productos al socket del cliente
+        // Recibe el producto recopilado del form y lo agrega al array de productos
+        socket.on("post", async data => {
+            const product = data;
+            await manager.addProduct(product);
+        })
+        // Recibe el id del producto y lo elimina del array de productos
+        socket.on("delete", async data => {
+            const pid = data;
+            await manager.deleteProduct(pid);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 app.use(express.json()); // Soporte para .json
 app.use(express.urlencoded({ extended: true })); // Soporte para params varios en las rutas
@@ -20,46 +49,8 @@ app.set("views", `${__dirname}/views`); // Ruta de la vistas
 app.set("view engine", "handlebars"); // Que motor se debe usar para renderizar
 
 // Routes
+app.use("/", viewsRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
-app.use("/", viewsRouter);
 
-const httpServer = app.listen(8080, () => console.log("Listening on port 8080"));
-const io = new Server(httpServer); // Creación de server Websockets
-// Conexión a base de datos con mongoose
-try {
-    await mongoose.connect("mongodb+srv://crgasparini32:GSNltCfSNvAcw1xd@cluster39760cg.jgxfm1z.mongodb.net/ecommerce?retryWrites=true&w=majority")
-} catch (error) {
-    console.log(error);
-}
-
-// Handshake entre servidor y socket del cliente
-io.on("connection", async socket => {
-    console.log("Nuevo cliente conectado");
-    const manager = new dbProductManager();
-    try {
-        const products = await manager.getProducts();
-        socket.emit("showProducts", products); // Envia los productos al socket del cliente
-    } catch (error) {
-        console.log(error);
-    }
-    // Recibe el producto recopilado del form y lo agrega al array de productos
-    socket.on("post", async data => {
-        try {
-            await manager.addProduct(data);
-        } catch (error) {
-            console.log(error);
-        }
-    })
-    // Recibe el id del producto y lo elimina del array de productos
-    socket.on("delete", async data => {
-        const pid = data;
-        try {
-            await manager.deleteProduct(pid);
-        } catch (error) {
-            console.log(error);
-        }
-    })
-})
-
-app.set("socketio", io);
+app.set('socketio', io);
