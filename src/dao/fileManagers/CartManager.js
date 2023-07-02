ea39236d761dc4a48cbce42fd0361788ef5dfa04
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-07-02 17:24:30 -0300

    refactor fileManagers

--- src/dao/fileManagers/CartManager.js
+++ src/dao/fileManagers/CartManager.js
@@ -1,74 +1,86 @@
 import fs from "fs";
-import ProductManager from "./ProductManager.js";
 
 export default class CartManager {
     constructor(path) {
         this.path = path;
     }
 
-    async getCarts() {
-        try {
-            // Validación de existencia del archivo de productos
-            if(fs.existsSync(this.path)){
-                const data = await fs.promises.readFile(this.path, "utf-8"); // Si existe, leo el archivo
-                const carts = JSON.parse(data); // Lo parseo a un array de objetos
-                return carts; // Retorno el array de productos
-            } else {
-                return []; // Si no existe, retorno un array vacio
-            }
-        } catch (error) {   
-            console.log(error);
+    async getAll() {
+        if(fs.existsSync(this.path)){
+            const data = await fs.promises.readFile(this.path, "utf-8");
+            const carts = JSON.parse(data);
+            return carts;
+        } else {
+            return [];
         }
     }
     
-    async getCartById(cid) {
-        try {
-            const carts = await this.getCarts(); // Traigo el array de carritos
-            const cart = carts.find(cart => cart.id === cid); // Busco el carrito con el id que recibo por parámetro
-            return cart;
-        } catch (error) {
-            console.log(error);
-        }
+    getById = async (cid) => {
+        const carts = await this.getAll();
+        const cart = carts.find(cart => cart._id === cid);
+        return cart;
     }
 
-    async addCart(cart) {
-        try {
-            const carts = await this.getCarts(); // Traigo el array de carritos
-            // Genero el id autoincrementable
-            if(carts.length === 0){
-                cart.id = 1; // Si no existe ningun carrito en el archivo el id será 1
-            } else {
-                cart.id = carts[carts.length - 1].id + 1; // Si existen carritos en el array, busco el id del ultimo + 1
-            }
-            carts.push(cart); // Agrego el carrito al array
-            await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t")); // Creo un nuevo archivo .json que contenga el nuevo array de carritos        
-        } catch (error) {
-            console.log(error);
+    add = async (cart) => {
+        const carts = await this.getAll();
+
+        if(carts.length === 0){
+            cart._id = 1;
+        } else {
+            cart.id = carts[carts.length - 1]._id + 1;
         }
+
+        carts.push(cart);
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+        return true;
     }
 
-    async addProductInCart(cid,pid) {
-        try {
-            const carts = await this.getCarts(); // Traigo el array de carritos
-            const cartIndex = carts.findIndex(cart => cart.id === cid); // Busco el índice del carrito al que deseo agregar un producto
-            const product = new ProductManager().getProductById(pid); // Busco el producto que se desea agregar
-            if(cartIndex === -1){
-                return "Error: El carrito no existe";
-            }
-            if(!product){
-                return "Error: el producto que desea añadir no existe";
-            }
-            const cartToModify = carts[cartIndex]; // Obtengo el carrito a modificar
-            const productInCart = cartToModify.products.find(object => object.product.id === pid); // Busco si el producto que quiero agregar ya existe en el carrito
-            if(productInCart){
-                productInCart.quantity++; // Si existe, incremento su cantidad en 1
-            } else {
-                cartToModify.products.push({product:{id:pid}, quantity: 1}); // Si no existe, lo agrego con cantidad 1
-            }
-            await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t")); // Creo un nuevo archivo .json con el nuevo array de carritos
-            return true;
-        } catch (error) {
-            console.log(error);
+    addOne = async (cid,pid) => {
+        const carts = await this.getAll();
+        const cartIndex = carts.findIndex(cart => cart._id === cid);
+        const productIndex = carts[cartIndex].products.findIndex(object => object.product._id === pid);
+        
+        if(productIndex === -1) {
+            carts[cartIndex].products.push({product:{_id:pid}, quantity: 1})
+        } else {
+            carts[cartIndex].products[productIndex].quantity++;
         }
+
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+        return true;
+    }
+
+    deleteOne = async (cid, pid) => {
+        const carts = await this.getAll();
+        const cartIndex = carts.findIndex(cart => cart._id === cid);
+        const productIndex = carts[cartIndex].products.findIndex(object => object.product._id === pid);
+        carts[cartIndex].products.splice(productIndex, 1);
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+        return true;
+    }
+
+    addMany = async (cid, products) => {
+        const carts = await this.getAll();
+        const cartIndex = carts.findIndex(cart => cart._id === cid);
+        cart[cartIndex].products = products;
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+        return true;
+    }
+
+    updateQuantityOne = async (cid, pid, quantity) => {
+        const carts = await this.getAll();
+        const cartIndex = carts.findIndex(cart => cart._id === cid);
+        const productIndex = carts[cartIndex].products.findIndex(object => object.product._id === pid);
+        carts[cartIndex].products[productIndex].quantity = quantity;
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+        return true;
+    } 
+
+    deleteAll = async (cid) => {
+        const carts = await this.getAll();
+        const cartIndex = carts.findIndex(cart => cart._id === cid);
+        carts[cartIndex].products = [];
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+        return true;
     }
 }
--- src/dao/fileManagers/ProductManager.js
+++ src/dao/fileManagers/ProductManager.js
@@ -5,102 +5,52 @@ export default class ProductManager {
         this.path = path;
     }
 
-    async getProducts() {
-        try {
-            // Validación de existencia del archivo de productos
-            if(fs.existsSync(this.path)){
-                const data = await fs.promises.readFile(this.path, "utf-8"); // Si existe, leo el archivo
-                const products = JSON.parse(data); // Lo parseo a un array de objetos
-                return products; // Retorno el array de productos
-            } else {
-                return []; // Si no existe, retorno un array vacio
-            }
-        } catch (error) {
-            console.log(error);
+    getAll = async () => {
+        if(fs.existsSync(this.path)){
+            const data = await fs.promises.readFile(this.path, "utf-8");
+            const products = JSON.parse(data);
+            return products;
+        } else {
+            return [];
         }
     }
 
-    async addProduct(product) {
-        try {
-            const products = await this.getProducts();
-            product.status = true; // Aplico parametro status valor true por defecto
-            let { title, price, stock, description, category, code } = product; // Desestructuro el producto recibido por parámetro
-            // Validación de los parámetros del producto
-            if(!title || !price || !stock || !description || !category || !code){
-                return "Error: Debe completar todos los campos obligatorios";
-            }
-            if(price <= 0){
-                return "Error: El precio no puede ser menor a 1";
-            }
-            if(stock <= 0){
-                return "Error: El stock no puede ser menor a 1";
-            }
-            // Conversión a number de stock y price
-            if(typeof price === "string"){
-                product.price = parseFloat(price);
-            }
-            if(typeof stock === "string"){
-                product.stock = parseInt(stock);
-            }
-            // Validación del code
-            const prodIndex = products.findIndex(prod => prod.code === code);
-            if(prodIndex !== -1){
-                return "Error: El código ingresado ya existe";
-            }
-            // Genero el id autoincrementable
-            if(products.length === 0){
-                product.id = 1; // Si no existe ningun producto en el archivo el id será 1
-            } else {
-                product.id = products[products.length - 1].id + 1; // Si existen productos busco el id del ultimo producto del array + 1
-            }
-            products.push(product); // Agrego al array de productos el nuevo producto
-            await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t")); // Creo un nuevo archivo .json que contenga el nuevo array de productos modificado
-            return true;
-        } catch (error) {
-            console.log(error);
+    add = async (product) => {
+        const products = await this.getAll();
+        products.status = true;
+
+        if(products.length === 0){
+            product.id = 1;
+        } else {
+            product.id = products[products.length - 1].id + 1;
         }
+        
+        products.push(product);
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
+        return true;
     }
 
-    async getProductById(pid) {
-        try {
-            const products = await this.getProducts(); // Traigo el array de productos
-            const product = products.find(prod => prod.id === pid); // Busco el producto en el array de productos
-            return product;
-        } catch (error) {
-            console.log(error);
-        }
+    getById = async (pid) => {
+        const products = await this.getAll();
+        const product = products.find(prod => prod.id === pid);
+        return product;
     }
 
-    async updateProduct(pid, modification) {
-        try {
-            const products = await this.getProducts(); // Traigo el array de productos
-            const productIndex = products.findIndex(prod => prod.id === pid); // Busco el índice del producto que deseo modificar
-            console.log(productIndex)
-            if(productIndex === -1){
-                return "Error: El producto que desea modificar no existe";
-            }
-            const productToModify = products[productIndex]; // Si existe, lo traigo
-            const modifiedProduct = Object.assign(productToModify, modification); // Creo el producto con las modificaciones
-            products.splice(productIndex, 1,modifiedProduct); // Reemplazo el producto original por el modificado
-            await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t")); // Creo un nuevo archivo .json con la modificación
-            return true
-        } catch (error) {
-            console.log(error);
-        }
+    update = async (pid, product) => {
+        const products = await this.getAll();
+        const productIndex = products.findIndex(prod => prod.id === pid);
+        const productToModify = products[productIndex];
+        const modifiedProduct = Object.assign(productToModify, product);
+        products.splice(productIndex, 1,modifiedProduct);
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
+        return true;
     }
 
-    async deleteProduct(pid) {
-        try {
-            const products = await this.getProducts(); // Traigo el array de productos
-            const productIndex = products.findIndex(prod => prod.id === pid); // Busco el índice del producto que deseo modificar
-            if(productIndex === -1){
-                return "Error: El producto que desea eliminar no existe";
-            }
-            products.splice(productIndex, 1); // Si existe, lo elimino del arreglo
-            await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t")); // Creo un nuevo archivo .json con la modificación
-            return true;
-        } catch (error) {
-            console.log(error);
-        }
+    delete = async (pid) => {
+        const products = await this.getAll();
+        const productIndex = products.findIndex(prod => prod.id === pid);
+        products.splice(productIndex, 1);
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
+        return true;
     }
 }
