3c8e809a8bf27437bb8d003e0c419f71b2befdc7
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-08-15 20:23:35 -0300

    implem swagger y documentacion de products y carts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
-import { __dirname } from "./path.js";
+import { __mainDirname } from "./utils/utils.js";
 import ProductsRouter from "./routes/products.router.js";
 import CartsRouter from "./routes/carts.router.js";
 import ViewsRouter from "./routes/views.router.js";
@@ -14,9 +14,13 @@ import config from "./config/config.js";
 import cors from "cors";
 import compression from "express-compression";
 import { logger, addLogger } from "./utils/logger.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
 
 const app = express(); // Creación de server HTTP
-const httpServer = app.listen(config.port, () => logger.info(`Listening on port ${config.port}`));
+const httpServer = app.listen(config.port, () =>
+  logger.info(`Listening on port ${config.port}`)
+);
 const io = new Server(httpServer); // Creación de server Websockets
 const sessionsRouter = new SessionsRouter();
 const cartsRouter = new CartsRouter();
@@ -25,35 +29,37 @@ const viewsRouter = new ViewsRouter();
 const logsRouter = new LogsRouter();
 
 // Handshake entre servidor y socket del cliente
-io.on("connection", async socket => {
-    logger.info("Nuevo cliente conectado");
-    try {
-        const messagesModule = await import("./services/messages.service.js");
+io.on("connection", async (socket) => {
+  logger.info("Nuevo cliente conectado");
+  try {
+    const messagesModule = await import("./services/messages.service.js");
 
-        socket.on("message", async data => {
-            await messagesModule.saveMessage(data);
-            io.emit("messageLogs", await messagesModule.getMessages());
-        });
-
-        socket.on("authenticated", async data => {
-            const messages = await messagesModule.getMessages();
-            socket.emit("messageLogs", messages);
-            socket.broadcast.emit("newUserConnected", data);
-        })
-    } catch (error) {
-        logger.error(error.message);
-    }
-})
+    socket.on("message", async (data) => {
+      await messagesModule.saveMessage(data);
+      io.emit("messageLogs", await messagesModule.getMessages());
+    });
+    
+    socket.on("authenticated", async (data) => {
+      const messages = await messagesModule.getMessages();
+      socket.emit("messageLogs", messages);
+      socket.broadcast.emit("newUserConnected", data);
+    });
+  } catch (error) {
+    logger.error(error.message);
+  }
+});
 
 app.set("socketio", io);
 app.use(express.json()); // Soporte para .json
 app.use(express.urlencoded({ extended: true })); // Soporte para params varios en las rutas
-app.use(express.static(`${__dirname}/public`)); // Acceso a archivos estáticos
+app.use(express.static(`${__mainDirname}/public`)); // Acceso a archivos estáticos
 app.use(cookieParser());
 app.use(cors());
-app.use(compression({
-    brotli: { enabled: true, zlib: {} }
-})); // aplicacion de compresion con brotli
+app.use(
+  compression({
+    brotli: { enabled: true, zlib: {} },
+  })
+  ); // aplicacion de compresion con brotli
 app.use(addLogger);
 
 // Passport
@@ -62,13 +68,26 @@ app.use(passport.initialize());
 
 // Configuración de handlebars
 app.engine("handlebars", handlebars.engine()); // Inicializa el motor
-app.set("views", `${__dirname}/views`); // Ruta de la vistas
+app.set("views", `${__mainDirname}/views`); // Ruta de la vistas
 app.set("view engine", "handlebars"); // Que motor se debe usar para renderizar
 
+// Configuración de swagger
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Documentación del proyecto de e-commerce para el curso de Backend de CoderHouse",
+      description: "API pensada para resolver el proceso de compra de productos online"
+    }
+  }, 
+  apis: [`${__mainDirname}/docs/**/*.yaml`]
+};
+
 // Routes
+const specs = swaggerJsdoc(swaggerOptions);
+app.use("/api/docs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 app.use("/", viewsRouter.getRouter());
 app.use("/api/products", productsRouter.getRouter());
 app.use("/api/carts", cartsRouter.getRouter());
 app.use("/api/sessions", sessionsRouter.getRouter());
 app.use("/api/logs", logsRouter.getRouter());
-
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import path from "path";
 import multer from "multer";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -11,79 +11,82 @@ faker.locale = "es";
 
 // Convención nomenclatura: para temas de paths __nombre
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+const __mainDirname = path.join(__dirname, "..");
 
 // Implementación de multer
 // Almacenamiento:
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `${__dirname}/public/img`);
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+  destination: (req, file, cb) => {
+    cb(null, `${__mainDirname}/public/img`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
 // Funciones de bcrypt
-const createHash = password => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
+const createHash = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
 
 const isValidPassword = (user, password) => {
-    return bcrypt.compareSync(password, user.password);
-}
+  return bcrypt.compareSync(password, user.password);
+};
 
 // jwt
 const generateToken = (user) => {
-    const token = jwt.sign({ user }, PRIVATE_KEY, {expiresIn: "1h"} );
-    return token;
-}
+  const token = jwt.sign({ user }, PRIVATE_KEY, { expiresIn: "1h" });
+  return token;
+};
 
 // Middlewares:
 const uploader = multer({
-    storage,
-    onError: (err, next) => {
-        logger.error(err),
-        next();
-    }
+  storage,
+  onError: (err, next) => {
+    logger.error(err), next();
+  },
 });
 
 const parseToNumber = (req, res, next) => {
-    const keys = ["price", "stock", "age"];
-    for(let key in req.body){
-        if(keys.includes(key)) {
-            req.body[key] = Number(req.body[key]);
-        }
+  const keys = ["price", "stock", "age"];
+  for (let key in req.body) {
+    if (keys.includes(key)) {
+      req.body[key] = Number(req.body[key]);
     }
-    next();
-}
+  }
+  next();
+};
 
 // Generador de mocks de productos
 const generateProducts = () => {
-    const numberOfImages = faker.random.numeric(1,{ allowLeadingZeros: false, bannedDigits: ["4","5","6","7","8","9"]});
-    const thumbnails = []
-    for(let i = 0; i < numberOfImages; i++) {
-        thumbnails.push(faker.image.image());
-    }
-    return {
-        title: faker.commerce.productName(),
-        description: faker.commerce.productDescription(),
-        price: faker.commerce.price(),
-        code: faker.random.alphaNumeric(6),
-        stock: faker.random.numeric(1),
-        category: faker.helpers.arrayElement(["consolas", "juegos", "accesorios"]),
-        status: true,
-        thumbnails,
-        _id: faker.database.mongodbObjectId()
-    }
-}
+  const numberOfImages = faker.random.numeric(1, {
+    allowLeadingZeros: false,
+    bannedDigits: ["4", "5", "6", "7", "8", "9"],
+  });
+  const thumbnails = [];
+  for (let i = 0; i < numberOfImages; i++) {
+    thumbnails.push(faker.image.image());
+  }
+  return {
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: faker.commerce.price(),
+    code: faker.random.alphaNumeric(6),
+    stock: faker.random.numeric(1),
+    category: faker.helpers.arrayElement(["consolas", "juegos", "accesorios"]),
+    status: true,
+    thumbnails,
+    _id: faker.database.mongodbObjectId(),
+  };
+};
 
 export {
-    __dirname,
-    uploader,
-    parseToNumber,
-    createHash,
-    isValidPassword, 
-    generateToken,
-    generateProducts
+  __mainDirname,
+  uploader,
+  parseToNumber,
+  createHash,
+  isValidPassword,
+  generateToken,
+  generateProducts,
 };
