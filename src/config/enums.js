8d2ccb2b88ba0538aa38b5ffd67c8703656c38d4
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-09-15 03:54:54 -0300

    elim: login con github

--- src/config/enums.js
+++ src/config/enums.js
@@ -3,7 +3,6 @@ enumeracion de estrategias de passport, en un objeto, en vez de usar un switch c
 */
 
 const passportStrategiesEnum = {
-    GITHUB: "github",
     JWT: "jwt",
     NOTHING: "na" //Caso que no se este aplicando ninguna estrategia
 }
--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,9 +1,6 @@
 import passport from "passport";
 import jwt from 'passport-jwt';
-import GitHubStrategy from "passport-github2";
 import { PRIVATE_KEY } from "./constants.js";
-import * as usersServicesModule from "../services/users.service.js";
-import config from "../config/config.js";
 
 const JWTStrategy = jwt.Strategy;
 const ExtractJWT = jwt.ExtractJwt;
@@ -20,35 +17,6 @@ const initializePassport = () => {
             return done(error);
         }
     }))
-
-    // registro/login con estrategia github
-    passport.use("github", new GitHubStrategy({
-        clientID: "Iv1.cd735444c94379d6",
-        clientSecret: "32496b7687db27d5fdd6ab6b0d71b601d4a520d3",
-        callbackURL: `http://localhost:${config.port}/api/sessions/github-callback`,
-        scope: ["user:email"]
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
-            const email = profile.emails[0].value;
-            const user = await usersServicesModule.getUser(email);
-            if(!user){
-                const newUser = {
-                    first_name: profile._json.name || profile.username,
-                    last_name: "",
-                    email,
-                    age: 18,
-                    password: ""
-                }
-                const result = await usersServicesModule.createUser(newUser);
-                done(null, result);
-            } else {
-                done(null, user);
-            }
-        } catch (error) {
-            done(`Error al registar el usuario: ${error}`);
-        }
-    }))
-
 }
 
 const cookieExtractor = req => {
